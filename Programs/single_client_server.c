1dd1a11e2c3ea1ae3a86f243cd48498d00f13d19
Tashik <k180142@nu.edu.pk>
2021-03-28 13:38:25 +0500

    Added single client server

---
+++ Programs/single_client_server.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <ctype.h>
+
+int main(int argc, char const *argv[])
+{
+    int port = atoi(argv[1]);
+    /* Describing socket */
+    int Server_Socket_File_Descripter = socket(AF_INET , SOCK_STREAM, 0);
+    if(!Server_Socket_File_Descripter)
+    {
+        printf("\nSorry! can't open server socket in server process, Please try again.");
+        exit(1);
+    }
+    /* Creating complete socket of InterNet domain*/
+    struct sockaddr_in Server_Address;
+    Server_Address.sin_family = AF_INET;
+    Server_Address.sin_port = htons(port);
+    Server_Address.sin_addr.s_addr = htonl(INADDR_ANY);
+    /* Binding */
+    if( bind(Server_Socket_File_Descripter, (struct sockaddr*)&Server_Address, sizeof(Server_Address))  <  0)
+    {
+        printf("\nSocket Binding Fails, Please Try Again.");
+        exit(2); 
+    }
+    while(1)
+    {
+        /* Making server ready to listen */
+        if( listen(Server_Socket_File_Descripter,1000) ) // returns '0' when there's no problem.
+        // Backlog of total 1000 clients. 1000 clients can be connected.
+        {
+            printf("\nAn Error Occured During Listening, Please Try Again.");
+            exit(3);
+        }
+        printf("... Server started listening on port: %d ...", port);
+
+        /* Setting socket options to avoid errors*/
+        int Option_Value = 1;
+        setsockopt(Server_Socket_File_Descripter, SOL_SOCKET, SO_REUSEADDR, (const void *)&Option_Value, sizeof(int));
+        /* Creating client socket address*/
+        struct sockaddr_in Client_Address;
+        char Buffer[1024]; /* 1 MB limit */
+        int Client_Address_Length = 0;
+        int Client_File_Descripter = 0;
+        int Read_Length = 0;
+        /* Accepting client connections */
+        Client_Address_Length = sizeof(Client_Address);
+        Client_File_Descripter = accept(Server_Socket_File_Descripter, (struct sockaddr*)&Client_Address, &Client_Address_Length);
+        if( Client_File_Descripter < 0 )
+        {
+            printf("\nError in accepting client connection request");
+            continue; // To retry
+        }
+        printf("\nConnected with %s:%d", inet_ntoa(Client_Address.sin_addr), ntohs(Client_Address.sin_port));
+        while(1)
+        {
+            Read_Length = recv(Client_File_Descripter, &Buffer, sizeof(Buffer), 0); 
+            if( Read_Length < 0 )
+            {
+                printf("\nAn Error occured while recieving data.");
+                exit(4);
+            }
+            if( Buffer[0]=='q' && strlen(Buffer)==2 )
+            {
+                break;
+            }
+            printf("\nServer Recieved Message From Client --> %s",Buffer);
+            printf("\nEnter a message to send it to the client:");
+            fgets(Buffer,1024,stdin);
+            int Sending_Status = send(Client_File_Descripter, &Buffer, sizeof(Buffer), 0);
+            if( Sending_Status < 0 )
+            {
+                printf("\nAn Error occured while sending data from server to client");
+            }
+        }
+    }
+    return 0;
+}
